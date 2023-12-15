196b5250d6388eb9919c920aa4708eb5e4113ae3
RuslanTsitser <cicer_ruslan@mail.ru>
2023-12-15 12:20:31 +0300

    raw unmodified

--- lib/src/floodfill_painter.dart
+++ lib/src/floodfill_painter.dart
@@ -34,7 +34,7 @@ class FloodFillPainter extends CustomPainter {
   }
 
   void _initFloodFiller() async {
-    ByteData byteData = (await image.toByteData(format: ui.ImageByteFormat.rawExtendedRgba128))!;
+    ByteData byteData = (await image.toByteData(format: ui.ImageByteFormat.rawUnmodified))!;
     var bytes = byteData.buffer.asUint8List();
     img.Image decoded = img.decodeImage(bytes)!;
     _filler = QueueLinearFloodFiller(decoded, fillColor);
